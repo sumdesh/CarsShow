17a352da231b28dc93e85a4ae401a8e9db953223
sumdesh <sumedhdeshpande@gmail.com>
2015-06-23 17:25:21 +0200

    starting default brach with intial refactoring

--- app/src/main/java/com/demotask/carsshow/activities/MainActivity.java
+++ app/src/main/java/com/demotask/carsshow/activities/MainActivity.java
@@ -1,22 +1,20 @@
-package com.demotask.carsshow;
+package com.demotask.carsshow.activities;
 
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
+import com.demotask.carsshow.NavigationDrawerFragment;
+import com.demotask.carsshow.R;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
